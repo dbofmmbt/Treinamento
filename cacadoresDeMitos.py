9d0910fa02661bd97fb964921627126b6f14d8b2
Eduardo Canellas de Oliveira <eduardocanellas@id.uff.br>
2018-04-25 22:03:09 -0300

    Problema 'Caçadores de Mitos' resolvido.

--- cacadoresDeMitos.py
+++ cacadoresDeMitos.py
@@ -1,16 +1,12 @@
 #Programa para a resolução do problema "Caçadores de Mitos":
-
-nTestes = int(input())
-listaX = [-1]
-listaY = [-1]
-fimdoprograma, contador, resposta, j = 0, 0, 0, 0
-
-for j in range(nTestes):
-	x,y = map(int, input().split())
-	for i in range(len(listaX)):
-		if listaX[i] == x and listaY[i] == y:
-					resposta = 1
-	listaX.append(x)
-	listaY.append(y)
-
+resposta = 0
+matriz = [[False] * 510 for i in range(510)]
+total = int(input())
+for i in range(total):
+	x, y = map(int, input().split())
+	if matriz[x][y]:
+		resposta = 1
+		break
+	else:
+		matriz[x][y] = True
 print(resposta)
