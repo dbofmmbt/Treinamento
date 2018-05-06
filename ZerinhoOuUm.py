7b40a07ce0146f20c948477c6e9f965c42a7e9b6
eduardocanellas <34381457+eduardocanellas@users.noreply.github.com>
2018-05-06 09:27:33 -0300

    Lista A2 concluída.

---
+++ ZerinhoOuUm.py
@@ -0,0 +1,14 @@
+EOF = False
+while not EOF:
+    try:
+        A, B, C = map(int, input().split())
+        if A != B and A != C:
+            print("A")
+        elif B != A and B != C:
+            print("B")
+        elif C != A and C != B:
+            print("C")
+        else:
+            print("*")
+    except EOFError:
+        EOF = True
