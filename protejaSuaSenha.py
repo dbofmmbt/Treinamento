7e0d80140f2bf81384f33cd363ea8dbe06ddc344
eduardocanellas <34381457+eduardocanellas@users.noreply.github.com>
2018-05-06 14:05:53 -0300

    "Proteja sua Senha" resolvido.
    Após um replanejamento da execução do algoritmo, finalmente o resultado desejado foi alcançado, embora alguns erros envolvendo listas tenham atrapalhado bastante.

--- protejaSuaSenha.py
+++ protejaSuaSenha.py
@@ -1,79 +1,74 @@
 # Funções e declarações:
+
 def resposta(teste, resposta):
     print("Teste", teste)
     for i in range(5):
         print(resposta[i], end=" ")
-    print(resposta[5], "\n", sep="")
-'''
-def verificarSenha(listaLetra, posicao1, posicao2, letra):
+    print(resposta[5],"\n")
 
-    for i in range(len(listaLetra)):
-        if lista[posicao1] == A[i]:
-            if i % 2 == 0:
-                if lista[i + 1] != lista[posicao2]:
-                    mapaDigitoLetra[letra] = lista[posicao1]
-            else:
-                if lista[i - 1] != lista[posicao2]:
-                    mapaDigitoLetra[letra] = lista[posicao1]
-
-        elif lista[posicao2] == A[i]:
-            if i % 2 == 0:
-                if lista[i + 1] != lista[posicao1]:
-                    mapaDigitoLetra[letra] = lista[posicao2]
-            else:
-                if lista[i - 1] != lista[posicao1]:
-                    mapaDigitoLetra[letra] = lista[posicao2]
-'''
-def calcularDigito(digito):
-    listaNumeros = []
-    for i in range((len(senha)//6)):
-        letra = senha[6 % digito * (i+1)]
-        if letra == 'A':
-            listaNumeros.append(A[(i * 2):(i * 2 + 1)])
-        elif letra == 'B':
-            listaNumeros.append(B[(i * 2):(i * 2 + 1)])
-        elif letra == 'C':
-            listaNumeros.append(C[(i * 2):(i * 2 + 1)])
-        elif letra == 'D':
-            listaNumeros.append(D[(i * 2):(i * 2 + 1)])
-        else:
-            listaNumeros.append(E[(i * 2):(i * 2 + 1)])
-
-
-def descobreNumero(listaLetra, pos1, pos2):
-    for i in range(len(listaLetra)):
-        if lista[pos1] == listaLetra[i]:
-            if i % 2 == 0:
-                if lista[i + 1] != lista[pos2]:
-                    mapaDigitoLetra['A'] = lista[0]
-            else:
-                if lista[i - 1] != lista[pos2]:
-                    mapaDigitoLetra['A'] = lista[0]
-
-        elif lista[pos2] == A[i]:
-            if i % 2 == 0:
-                if lista[i + 1] != lista[pos1]:
-                    mapaDigitoLetra['A'] = lista[1]
-            else:
-                if lista[i - 1] != lista[pos1]:
-                    mapaDigitoLetra['A'] = lista[1]
-
+def descobreNumero(dig):
+    
+    for i in dig:
+        Achou =  True
+        for j in range(0, len(dig), 2):
+            if i != dig[j] and i != dig[j+1]:
+                Achou = False
+        if Achou:
+            return i
+        
+def listarDigito(dig, numero):
+    if senha[numero] == 'A':
+        dig.append(A[0])
+        dig.append(A[1])
+    elif senha[numero] == 'B':
+        dig.append(B[0])
+        dig.append(B[1])
+    elif senha[numero] == 'C':
+        dig.append(C[0])
+        dig.append(C[1])
+    elif senha[numero] == 'D':
+        dig.append(D[0])
+        dig.append(D[1])
+    else:
+        dig.append(E[0])
+        dig.append(E[1])
 
 lista = []
-senha = []
-mapaDigitoLetra = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0}
-A, B, C, D, E = [], [], [], [], []
 
+resultado = []
+nTeste = 1
 # Início do programa "Proteja sua Senha!":
 nLinhas = int(input())
 while nLinhas != 0:
+    dig1, dig2, dig3, dig4, dig5, dig6 = [], [], [], [], [], []   
     for i in range(nLinhas):
+        A = B = C = D = E = []    
+        senha = []
         lista = input().split()
+        numeros = lista[0:10]
+        A = lista[0:2]
+        B = lista[2:4]
+        C = lista[4:6]
+        D = lista[6:8]
+        E = lista[8:10]
+        for j in range(10, 16):
+            senha.append(lista[j])
 
-        A.append(lista[0:2])
-        B.append(lista[2:4])
-        C.append(lista[4:6])
-        D.append(lista[6:8])
-        E.append(lista[8:10])
-        senha.append(lista[10:16])
+        listarDigito(dig1, 0)
+        listarDigito(dig2, 1)
+        listarDigito(dig3, 2)
+        listarDigito(dig4, 3)
+        listarDigito(dig5, 4)
+        listarDigito(dig6, 5)
+
+    resultado.append(descobreNumero(dig1))
+    resultado.append(descobreNumero(dig2))
+    resultado.append(descobreNumero(dig3))
+    resultado.append(descobreNumero(dig4))
+    resultado.append(descobreNumero(dig5))
+    resultado.append(descobreNumero(dig6))
+    
+    resposta(nTeste, resultado)
+    resultado = []
+    nTeste += 1
     nLinhas = int(input())
