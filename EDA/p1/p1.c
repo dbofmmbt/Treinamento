d8f19edaa128258e4f62815df3d714d16e3fa36d
Eduardo Canellas de Oliveira <eduardocanellas@id.uff.br>
2019-07-08 20:55:11 -0300

    p1

---
+++ EDA/p1/p1.c
@@ -0,0 +1,21 @@
+/*
+
+1. Dada uma árvore binária que respeita as seguintes propriedades:
+
+    * Todo pai que tem uma altura ímpar possui 1 filho;
+    * Todo pai que tem uma altura par possui 2 ou 0 filhos;
+
+    Faça uma função que, dada uma árvore, cheque se ela respeita essas propriedade.
+    assinatura: int arv_esp(TAB *a);
+
+
+2. Faça uma função que, dado um grafo, retorna o número de conjuntos conexos que esse
+    grafo possui.
+    assinatura: int cc(TG *g);
+    (Ela dava algumas funções e a estrutura do grafo já pronta)
+
+3. Ela pedia pra gente fazer em desenho os algoritmos de Geração de Partições Classificadas
+
+4. Desenho de rotação de AVL
+
+*/
